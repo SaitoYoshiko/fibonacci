630adfaec0e48c56430f64d01dc10040f948668c
SaitoYoshiko <yo.saito3759@icloud.com>
2021-06-06 12:02:42 +0900

    commit

--- cookie-study/index.js
+++ cookie-study/index.js
@@ -9,7 +9,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   const now = Date.now();
   res.setHeader('Set-Cookie', 'last_access=' + now + ';');
-  const last_access_time = req.header.cookie ? parseInt(req.header.cookie.split('last_access=')[1]) : now;
+  const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
   res.end(new Date(last_access_time).toString());
 });
 
