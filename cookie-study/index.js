cd2321b23a1ebeb2c0ed2989bf9808bc842e570d
SaitoYoshiko <yo.saito3759@icloud.com>
2021-06-06 11:07:47 +0900

    マージ

--- cookie-study/index.js
+++ cookie-study/index.js
@@ -3,17 +3,18 @@
  * 　httpのCookieを利用するサーバを作ります
  */
 
+
 'use strict'
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = Date.now();
-  res.setHeader('Set-Cookie', 'last-access=' + now + ';');
-  const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
+  const last_access_time = req.header.cookie ? parseInt(req.header.cookie.split('last_access=')[1]) : now;
   res.end(new Date(last_access_time).toString());
 });
+
 const port = 8000;
 server.listen(port, () => {
-  console.info('Listening on ' + port);
+  console.info('Listening on :' + port);
 });
 
 
